462ac55294e8637fd15b587f29d2aad3aaa35a60
voigtito <voigt.gustavoh@gmail.com>
2021-04-11 18:51:16 -0300

    Adding miragejs and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import {GlobalStyle} from './styles/global';
 import {Header} from './components/Header';
+import { createServer } from 'miragejs';
 import {Dashboard} from './components/Dashboard';
+
+createServer({
+  routes() {
+    this.namespace = 'api'; // All calls get this base name.
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 export function App() {
   return (
